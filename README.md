07a31a3bc4c9447af9d61307b862cd3b9c3c9cd2
Mark Brooks <mbrooks@fluentstream.com>
2020-09-02 09:34:04 -0600

    update readme and remove duplicate loading guy

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # rona
+Please run with Node v11.10.1
+`nvm use 11.10.1`
+
+To start on localhost
+`npm start`
--- components/CountrySelector.js
+++ components/CountrySelector.js
@@ -9,7 +9,6 @@ export default function CountrySelector(){
       );
     const [selectedCountry, setSelectedCountry] = useState('USA');
         if (loading) return <p>Loading...</p>;
-        if (loading) return <p>Loading...</p>;
         if (error) return <p>Error...</p>;
     return (
         <div>
